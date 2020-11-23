3ed8a7a5a0831a80e83228390a33085f8ec95988
MasterJH5574 <45167100+MasterJH5574@users.noreply.github.com>
2020-11-23 15:25:27 +0800

    [tensorize support] v4.2 * mma_ldmatrix_x2_float() bug fix

--- src/target/source/codegen_cuda.cc
+++ src/target/source/codegen_cuda.cc
@@ -112,7 +112,7 @@ std::string CodeGenCUDA::Finish() {
                      "    \"}\\n\"\n"
                      "    : \"=r\"(fragment[0]), "
                      "\"=r\"(fragment[1])\n"
-                     "    : \"l\"(shared_mem_ptr + threadIdx.x % 8 * strides)\n"
+                     "    : \"l\"(shared_mem_ptr + threadIdx.x % 16 * strides)\n"
                      "  );\n"
                      "}\n\n";
       decl_stream << "__device__ inline void mma_sync_m16n8k8_161632(float * fragmentD, "
